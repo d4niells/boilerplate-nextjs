9ecb2eb3e39041e9a3d87586c1432651f14624bb
Daniel Oliveira <danieloliver.dev@gmail.com>
2021-09-03 21:24:25 -0300

    chore: add story controls

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, Story } from '@storybook/react';
+
+import Main from '.';
+
+export default {
+  title: 'Main',
+  component: Main
+} as Meta;
+
+export const Basic: Story = (args) => <Main {...args} />;
+
+Basic.args = {
+  title: 'React Avançado',
+  description: 'Typescript, ReactJS, NextJS e Styled Components'
+};
